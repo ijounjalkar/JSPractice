07b0124a22551a3ad697a269f39a8076007c93fc
ijounjalkar <jounjalkarisha@gmail.com>
2020-11-12 01:18:31 +0530

    Added functionality of operation of Unit Conversion.

---
+++ UnitConversion.js
@@ -0,0 +1,5 @@
+console.log("Conversion of 42 inches in fit is: "+42/12);
+let plotArea = (60*40)*0.3048;
+console.log("Rectangular Area is: " +(plotArea));
+let plotInAcres = plotArea * 0.000247105;
+console.log("Area of 25 such plots: " + 25*plotInAcres);
